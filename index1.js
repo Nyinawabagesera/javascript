61a6f6cce6846219b3a2ed2cf6d2badc5978772b
Nyinawabagesera <j.nyinawaba@alustudent.com>
2024-02-05 15:14:27 +0000

    files

--- index1.js
+++ index1.js
@@ -1,1 +1,3 @@
-javascript
+const colors = ['red', 'green', 'blue', 'yellow', 'orange'];
+// Using the join() method, create a string that joins all elements of the colors array into a single string separated by commas.
+console.log(colors.join(','));
