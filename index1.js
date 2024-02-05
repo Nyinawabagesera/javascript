251135dc85ab0c8180a41a1fe2e46df558ce0cc6
Nyinawabagesera <j.nyinawaba@alustudent.com>
2024-02-05 15:59:45 +0000

    file

--- index1.js
+++ index1.js
@@ -5,5 +5,22 @@ const colors = ['red', 'green', 'blue', 'yellow', 'orange'];
 //console.log(colors.splice(2,2,'purple','pink'));
 //console.log(colors);
 // Utilize the copyWithin() method to copy the elements from index 1 to index 3 within the colors array to index 0.
-colors.copyWithin(0,1,3);
- console.log(colors);
+//colors.copyWithin(0,1,3);
+// console.log(colors);
+// Finally, transform all elements of the colors array to uppercase using the toUpperCase() method.
+
+//const uppercaseColors = colors.map(color => color.toUpperCase());
+
+//console.log(uppercaseColors);
+
+//const startsWithB = colors.find(color => color.startsWith('b'));
+
+//console.log(startsWithB);
+
+// Apply the slice() method to create a new array selectedColors containing the elements from index 1 to index 3 (inclusive) from the colors array.
+console.log(colors.slice(1,4));
+
+
+
+
+
